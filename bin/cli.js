f9657c8c7781f7de151f9ded58cfbda4f5e559fe
Bang Gong <gongbang@email.unc.edu>
2022-11-14 16:45:29 -0500

    cli.js added

---
+++ bin/cli.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import minimist from "minimist";
+import { roll } from "../lib/roll.js";
+
+const args = minimist(process.argv.slice(2));
+
+let sides = 6;
+let dice = 2;
+let rolls = 1;
+
+if(args.sides) sides = args.sides;
+if(args.dice) dice = args.dice;
+if(args.rolls) rolls = args.rolls;
+
+const output = roll(sides, dice, rolls);
+console.log(JSON.stringify(out));
